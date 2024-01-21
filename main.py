c7648fc1babb93977eb3dca645ad7f5352d1ab67
Dirk Schlussenbach <68381342+Dirk-Schlussenbach@users.noreply.github.com>
2024-01-21 13:44:48 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import torch
+from diffusers import AutoPipelineForText2Image
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+
+pipeline = AutoPipelineForText2Image.from_pretrained(
+	"runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16, variant="fp16").to("cuda")
+
+
+
+image = pipeline(
+	"stained glass of darth vader, backlight, centered composition, masterpiece, photorealistic, 8k").images[0]
+image
